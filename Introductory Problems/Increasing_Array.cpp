26608f1fc7915a4904383369045a267bf02f3d2f
casafurix <agnibha10@gmail.com>
2021-10-02 23:09:54 +0530

    added: Increasing Array

--- Introductory Problems/Increasing_Array.cpp
+++ Introductory Problems/Increasing_Array.cpp
@@ -1,7 +1,25 @@
 #include <bits/stdc++.h>
 using namespace std;
-
+#define ll long long
 int main()
 {
-    return 0;
+    ll n, diff;
+    ll arr[1000000];
+    ll sum = 0;
+    cin >> n;
+
+    for (ll i = 1; i <= n; i++)
+    {
+        cin >> arr[i];
+    }
+    for (ll i = 1; i <= n; i++)
+    {
+        if (arr[i] < arr[i - 1])
+        {
+            diff = arr[i - 1] - arr[i];
+            arr[i] += diff;
+            sum += diff;
+        }
+    }
+    cout << sum;
 }

15bf8a0092df87fb55306dd1c2da82c6ab3c95d6
casafurix <agnibha10@gmail.com>
2021-10-04 19:41:48 +0530

    added: till #7

---
+++ Introductory Problems/Number_Spiral.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+
+    string str;
+    ll t, y, x;
+    cin >> t;
+    for (int i = 0; i < t; i++)
+    {
+        cin >> y >> x;
+        if (x > y)
+        {
+            if (x % 2 == 1)
+            {
+                cout << x * x - y + 1;
+            }
+            else
+            {
+                x--;
+                cout << x * x + y;
+            }
+        }
+        else
+        {
+            if (y % 2 == 0)
+            {
+                cout << y * y - x + 1;
+            }
+            else
+            {
+                y--;
+                cout << y * y + x;
+            }
+        }
+        cout << "\n";
+    }
+
+    return 0;
+}
---
+++ Introductory Problems/Permutations.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+
+    ll n;
+    cin >> n;
+    string str1 = "";
+    if (n <= 3 && n != 1)
+    {
+        cout << "NO SOLUTION";
+        return 0;
+    }
+    for (int i = 1; i <= n; i++)
+    {
+        if (i % 2 == 0)
+        {
+            cout << i << " ";
+        }
+        else
+        {
+            str1 += to_string(i) + " ";
+        }
+    }
+    cout << str1;
+    return 0;
+}
